4663cfb5a140b3f91ac8a714e42965cae3ccb055
邓书东 <344887649@qq.com>
2025-04-09 22:28:16 +0800

    新增csdn屏蔽,将知乎的搜索内容居中

--- config.js
+++ config.js
@@ -1,28 +1,57 @@
 window.SITE_CONFIG = {
-  'www.bilibili.com': {
+  "www.bilibili.com": {
     targets: [
-      'main.bili-feed4-layout',
-      'div.bili-header__channel',
-      'div.header-channel'
+      "main.bili-feed4-layout",
+      "div.bili-header__channel",
+      "div.header-channel",
     ],
     extraCheck: (root) => {
-      const searchInput = root.querySelector('.nav-search-input');
+      const searchInput = root.querySelector(".nav-search-input");
       if (searchInput?.placeholder) {
-        searchInput.removeAttribute('placeholder');
+        searchInput.removeAttribute("placeholder");
       }
-    }
+    },
   },
-  'www.baidu.com': {
+  "www.baidu.com": {
     targets: [
-      'div.s-hotsearch-wrapper',
-      'div.new_search_guide_bub',
-      'div.cr-offset'
-    ]
+      "div.s-hotsearch-wrapper",
+      "div.new_search_guide_bub",
+      "div.cr-offset",
+    ],
   },
-  'www.zhihu.com': {
+  "www.zhihu.com": {
     targets: [
-      'div.Topstory',
-      'div.LoadingBar'
-    ]
-  }
+      "div.Topstory",
+      "div.LoadingBar",
+      "div.Card.TopSearch.TopSearch--new",
+      "div.css-knqde",
+      // 'div.AutoComplete-group'
+    ],
+    extraCheck: (root) => {
+      const searchInput = root.querySelector("#Popover1-toggle");
+      if (searchInput?.placeholder) {
+        searchInput.removeAttribute("placeholder");
+      }
+      const discoveryGroups = root.querySelectorAll(".AutoComplete-group");
+      discoveryGroups.forEach((group) => {
+        const label = group.querySelector(".SearchBar-label");
+        if (label?.textContent?.includes("搜索发现")) {
+          group.remove();
+        }
+      });
+      const searchMain = root.querySelector("#SearchMain");
+      if (searchMain) {
+        searchMain.style.width = "1000px"; // 设置行内样式优先级更高
+      }
+    },
+  },
+  "blog.csdn.net": {
+    targets: [
+      "div.swiper-slide-box-remuneration",
+      "div#asideHotArticle.aside-box",
+      "div#asideCategory.aside-box ",
+      "div#asideArchive.aside-box",
+      "div#asideNewComments.aside-box",
+    ],
+  },
 };
